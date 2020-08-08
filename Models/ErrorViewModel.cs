5d88930eecd627a61fad790ce6add2f261a58304
amilaprag <56796940+amilaprag@users.noreply.github.com>
2020-08-08 12:39:17 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InterfaceWEBHealthcare.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
