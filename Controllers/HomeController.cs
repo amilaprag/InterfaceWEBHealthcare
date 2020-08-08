b80b13dc1b99ba680988c1758110e79776aeb69b
amilaprag <56796940+amilaprag@users.noreply.github.com>
2020-08-09 01:54:47 +0530

    get petieqnt details postcall

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using InterfaceWEBHealthcare.Models;
+using System.Net.Http;
+using InterfaceWEBHealthcare.Models.Patient;
+using InterfaceWEBHealthcare.Models.Patient.PatientResponse;
 
 namespace InterfaceWEBHealthcare.Controllers
 {
@@ -20,7 +23,29 @@ namespace InterfaceWEBHealthcare.Controllers
 
         public IActionResult Index()
         {
-            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/EHealthCareAPI");
+
+                PatientMasterData PatientMasterDataObj = new PatientMasterData();
+                PatientMasterDataObj.Authkey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImFtaWxhcHJhZ2VldGhAZ21haWwuY29tIiwibmFtZWlkIjoiYW1pbGFwcmFnZWV0aEBnbWFpbC5jb20iLCJyb2xlIjoicGF0aWVudCIsIm5iZiI6MTU5Njg4NDgxNCwiZXhwIjoxNjAxNjg0ODE0LCJpYXQiOjE1OTY4ODQ4MTQsImlzcyI6Imh0dHA6Ly9teXNpdGUuY29tIiwiYXVkIjoiaHR0cDovL215YXVkaWVuY2UuY29tIn0.VtV9yxsVvGhOyfB55L0H3R0yvifVJOe-D28bpT_KpqM";
+                PatientMasterDataObj.Username ="";
+                PatientMasterDataObj.Password ="";
+                PatientMasterDataObj.TraceId = "";
+
+                var postTask = client.PostAsJsonAsync<PatientMasterData>("/Patient/ValidatePatientRS",PatientMasterDataObj);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var response = result.Content.ReadAsAsync<PatientResponse>();
+                    response.Wait();
+
+                    PatientResponse PatientResponseObj = response.Result;
+                    return View(PatientResponseObj);
+                }
+            }
             return View();
         }
         
