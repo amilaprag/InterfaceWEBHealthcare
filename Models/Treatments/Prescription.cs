8230155ed1d6fa9f57471b7f86ef684284ac956e
amilaprag <56796940+amilaprag@users.noreply.github.com>
2020-08-09 23:19:49 +0530

    treatements model initial changes

---
+++ Models/Treatments/Prescription.cs
@@ -0,0 +1,52 @@
+﻿using InterfaceWEBHealthcare.Models.Doctors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InterfaceWEBHealthcare.Models.Patient;
+//using InterfaceWEBHealthcare.Encryption;
+using System.ComponentModel.DataAnnotations.Schema;
+using InterfaceWEBHealthcare.Models.Appoinment;
+
+namespace InterfaceWEBHealthcare.Models.Treatments
+{
+    public class Prescription
+    {
+        private int prescriptionID;
+
+        private DateTime prescriptionDate;
+
+        private string diagnosis;
+
+
+        public int PrescriptionID { get => prescriptionID; set => prescriptionID = value; }
+        public DateTime PrescriptionDate { get => prescriptionDate; set => prescriptionDate = value; }
+        public string Diagnosis { get => diagnosis; set => diagnosis = value; }
+
+        //RelationShip between Medicine
+        public List<Medicine> Medicine { get; set; }
+
+
+        //RelationShip between Doctor
+        public int DoctorID { get; set; }
+        public Doctor Doctor { get; set; }
+
+        //RelationShip between Patient
+        public PatientMasterData Patient { get; set; }
+
+        //RelationShip between Key
+      //  public KeyInfo KeyInfo { get; set; }
+
+        //RelationShip between Key
+        public Appoinments Appoinment { get; set; }
+
+        // Mapping AuthKey and TraceID
+        private string authkey;
+        private string traceId;
+        [NotMapped]
+        public string Authkey { get => authkey; set => authkey = value; }
+        [NotMapped]
+        public string TraceId { get => traceId; set => traceId = value; }
+       
+    }
+}
---
+++ Models/Treatments/TreatmentResponse.cs
@@ -0,0 +1,48 @@
+﻿using InterfaceWEBHealthcare.Models.Doctors;
+using InterfaceWEBHealthcare.Models.Patient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterfaceWEBHealthcare.Models.Treatments
+{
+    public class TreatmentResponse
+    {
+        public PrescriptionResponse PrescriptionResponse { get;set;}
+    }
+
+    public class PrescriptionResponse
+    {
+        private int prescriptionID;
+
+        private DateTime prescriptionDate;
+
+        private int  doctorID;
+        private string doctorName;
+        private string doctoremail;
+        private string hospital;
+
+
+        public int PrescriptionID { get => prescriptionID; set => prescriptionID = value; }
+        public DateTime PrescriptionDate { get => prescriptionDate; set => prescriptionDate = value; }
+
+
+        public List<Medicine> Medicine { get; set; }
+        // public Doctor Doctor { get; set; }
+        public PatientMasterData Patient { get; set; }
+        public int DoctorID { get => doctorID; set => doctorID = value; }
+        public string DoctorName { get => doctorName; set => doctorName = value; }
+        public string Doctoremail { get => doctoremail; set => doctoremail = value; }
+        public string Hospital { get => hospital; set => hospital = value; }
+    }
+    public class TreatmentSucessResponse
+    {
+        private string status;
+
+        private string statusMessage;
+
+        public string Status { get => status; set => status = value; }
+        public string StatusMessage { get => statusMessage; set => statusMessage = value; }
+    }
+}
